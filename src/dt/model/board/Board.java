84d024e3c636b0f681390a4d37552fc734e2dcb6
Wouter Koning <wouter-koning@hotmail.nl>
2021-01-11 11:21:23 +0100

    Added testing libraries

--- src/dt/model/board/Board.java
+++ src/dt/model/board/Board.java
@@ -2,19 +2,35 @@ package dt.model.board;
 
 import dt.model.BallType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
 
-    private List<Row> rows;
-    private List<Column> columns;
+    private List<Sequence> rows;
+    private List<Sequence> columns;
     private final int boardSize;
 
     public Board(int boardSize){
         this.boardSize = boardSize;
+        rows = new ArrayList<>();
+        columns = new ArrayList<>();
+
+//        for(int i = 0; i < this.boardSize; i++){
+//            rows.add(new Row)
+//        }
+
     }
 
     public List<BallType> makeMove(int move){
+        if(move < 0 || move > 28){
+            //youDumbFuckException
+        } else if (move > 20) {
+            columns.get(move - 21).shiftLeftOrDown();
+        } else if (move > 13){
+
+        }
+
         return null;
     }
 
--- src/dt/model/board/Row.java
+++
@@ -1,8 +0,0 @@
-package dt.model.board;
-
-public class Row implements Sequence{
-    @Override
-    public void doMove(int move) {
-
-    }
-}
