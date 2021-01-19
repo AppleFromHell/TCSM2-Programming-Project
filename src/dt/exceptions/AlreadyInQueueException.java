763a5b5b9d7de2ce03d38722275517fb6c0ae5a4
Wouter Koning <wouter-koning@hotmail.nl>
2021-01-19 16:28:11 +0100

    Added @author javadoc to AlreadyInQueueException

--- src/dt/exceptions/AlreadyInQueueException.java
+++ src/dt/exceptions/AlreadyInQueueException.java
@@ -1,5 +1,6 @@
 package dt.exceptions;
 
+/** @author Emiel Rous and Wouter Koning */
 public class AlreadyInQueueException extends Throwable {
     public AlreadyInQueueException(String yer_already_in_a_queue) {
         super(yer_already_in_a_queue);
