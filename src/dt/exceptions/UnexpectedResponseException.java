9ebc68af3a07acfc90199e1aba811ba69b1239db
Wouter Koning <wouter-koning@hotmail.nl>
2021-01-19 16:24:14 +0100

    Added @author javadoc for Exception package

--- src/dt/exceptions/UnexpectedResponseException.java
+++ src/dt/exceptions/UnexpectedResponseException.java
@@ -1,5 +1,6 @@
 package dt.exceptions;
 
+/** @author Emiel Rous and Wouter Koning */
 public class UnexpectedResponseException extends Exception{
     public UnexpectedResponseException(String msg) {
         super(msg);
