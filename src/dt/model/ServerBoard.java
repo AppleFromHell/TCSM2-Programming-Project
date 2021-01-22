6f49fc5fdf0753927fd4068c446ccd36ac45a1f3
Wouter Koning <wouter-koning@hotmail.nl>
2021-01-22 23:29:56 +0100

    Attempted to do something but failed, and now created something that does something random. Victory!

--- src/dt/model/ServerBoard.java
+++ src/dt/model/ServerBoard.java
@@ -2,9 +2,8 @@ package dt.model;
 
 import dt.util.DistributedRandomNumberGenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /** @author Emiel Rous and Wouter Koning */
 public class ServerBoard extends Board{
@@ -63,8 +62,8 @@ public class ServerBoard extends Board{
         return availableNumbers.get(randomNumber(0, availableNumbers.size() - 1));
     }
 
+    //TODO Make it so that only 8 balls of every colour exist in the board (use the DistributedRandomNumberGenerator)
     private int[] createBoard() {
-        //TODO Make it so that only 8 balls of every colour exist in the board (use the DistributedRandomNumberGenerator)
         //fill the board up daddy
         int[] newBoard = new int[this.boardSize * this.boardSize];
         int middle = (this.boardSize * this.boardSize - 1) / 2;
@@ -78,9 +77,9 @@ public class ServerBoard extends Board{
             randomNumberGenerator.addNumber(i + 1, ballsLeft[i + 1]); //Add all the possible numbers to the generator.
         }
 
-        for (int i = 0; i < this.boardSize * this.boardSize; i++) { //i = 1
-            int left = i - 1; //left =0
-            int up = i - this.boardSize; //up = -6
+        for (int i = 0; i < this.boardSize * this.boardSize; i++) {
+            int left = i - 1;
+            int up = i - this.boardSize;
             if (i == middle) {
                 newBoard[i] = 0;
             } else if (i % this.boardSize != 0 && up >= 0) {   //If it's not on the left edge, and not at the top
@@ -99,5 +98,70 @@ public class ServerBoard extends Board{
         }
 
         return newBoard;
+    }
+
+    private int[] createBoard2(){
+
+        int[] newBoard = createRandomBoard();
+        boolean boardFucked = true;
+
+        while(!boardFucked) {
+            for (int i = 0; i < newBoard.length; i++) {
+                if (newBoard[i] == newBoard[i + 1]) {
+                    swap(newBoard, i, i + 1);
+                }
+                if (newBoard[i] == newBoard[i + 7]) {
+                    swap(newBoard, i, i + 7);
+                }
+            }
+        }
+
+        return newBoard;
+    }
+
+    private void swap(int[] array, int i1, int i2){
+        int temp = array[i2];
+        array[i2] = array[i1];
+        array[i1] = temp;
+    }
+
+    private int[] createRandomBoard(){
+        int[] newBoard = new int[this.boardSize * this.boardSize];
+        int middle = (this.boardSize * this.boardSize - 1) / 2;
+        List<BallType> ballList = List.of(BallType.values());
+        Map<BallType, Integer> ballCount = new HashMap<>();
+        for(BallType ball : ballList){
+            ballCount.put(ball, 8);
+        }
+        ballCount.remove(BallType.EMPTY);
+
+//        List<BallType> newBoard = new ArrayList<>();
+        for(int i = 0; i < this.boardSize * this.boardSize; i++){
+            if(i != middle) {
+                int randomNr = randomNumber(0, ballCount.keySet().size() - 1);
+                BallType randomKey = getFromSet(ballCount.keySet(), randomNr);
+                ballCount.put(randomKey, ballCount.get(randomKey) - 1);
+                if (ballCount.get(randomKey) == 0) {
+                    ballCount.remove(randomKey);
+                }
+                assert randomKey != null;
+                newBoard[i] = randomKey.ordinal();
+            } else {
+                newBoard[i] = 0;
+            }
+        }
+        return newBoard;
+    }
+
+    private BallType getFromSet(Set<BallType> set, int getIndex){
+        int i = 0;
+        for(Iterator<BallType> it = set.iterator(); it.hasNext(); ){
+            BallType ball = it.next();
+            if(i == getIndex){
+                return ball;
+            }
+            i++;
+        }
+        return null;
     }
 }
